e0f61937f702d5aa6bef22620a1106181b09c859
lokisk1155 <shawn12345d@gmail.com>
2023-10-20 17:31:31 -0400

    clean

--- app/inventory/page.tsx
+++ app/inventory/page.tsx
@@ -20,12 +20,12 @@ export interface SessionUser {
 export default function Page() {
   const session = useSession()
   const authenticated = session.status === 'authenticated' ? true : false
-  let title = adminPageHeaderProps.title
+  const title = adminPageHeaderProps.title
   let description = adminPageHeaderProps.description
 
   if (session.status === 'authenticated' && session.data.user) {
-    title = session.data.user.name || 'Not Found'
-    description = 'welcome back, access granted to view inventory records'
+    // @ts-ignore user.role is always added from jwt auth callback (api/auth/[...nextauth]/options.ts), not apart of native session
+    description = `signed in as ${session.data.user.name} | role: ${session.data.user.role}`
   }
 
   return (
