56f8853c457e7d103190de1624c064197fbd8ece
lokisk1155 <shawn12345d@gmail.com>
2023-10-23 08:42:01 -0400

    switch to swr in use client

--- app/fish/page.tsx
+++ app/fish/page.tsx
@@ -1,27 +1,31 @@
+'use client'
 import Loading from '@/components/Loading'
 import Timeline from './components/Timeline'
 import { SessionUser } from 'interfaces/session'
 import { PageHeader } from '@/components/PageHeader'
 import { fishPageHeaderProps } from '@/data/pageHeader'
-import { getServerSession } from 'next-auth'
-import { authOptions } from 'app/api/auth/[...nextauth]/options'
-import { FishRecord } from 'app/api/fish/route'
+import useSWR from 'swr'
+import { useSession } from 'next-auth/react'
 
 const requestUrl = process.env.NEXT_PUBLIC_URL + 'api/fish'
+const fetcher = (url: string) => fetch(url).then((res) => res.json())
 
-export default async function Page() {
-  const request = await fetch(requestUrl)
-  const fishDataArray: Array<FishRecord> = await request.json()
-  const session = await getServerSession(authOptions)
+export default function Page() {
+  const { data, error, isLoading } = useSWR(requestUrl, fetcher)
+  const session = useSession()
 
   return (
     <>
       <PageHeader title={fishPageHeaderProps.title} description={fishPageHeaderProps.description} />
       <div className="flex flex-col items-center space-x-2">
-        {fishDataArray.length ? (
+        {data && !isLoading ? (
           <Timeline
-            recordedFishData={fishDataArray}
-            user={session ? (session.user as SessionUser) : null}
+            recordedFishData={data}
+            user={
+              session && session.data && session.data.user
+                ? (session.data.user as SessionUser)
+                : null
+            }
           />
         ) : (
           <Loading />
