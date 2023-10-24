e1a63bcc774a39d6368e8f8e8429f793b207529c
lokisk1155 <shawn12345d@gmail.com>
2023-10-24 09:36:07 -0400

    remove not used file

--- data/roleManager.ts
+++
@@ -1,9 +0,0 @@
-import { Role } from 'interfaces/session'
-
-export function setWantedRole(role: Role) {
-  localStorage.setItem('wantedRole', role)
-}
-
-export function getWantedRole(): Role | null {
-  return localStorage.getItem('wantedRole') as Role | null
-}
