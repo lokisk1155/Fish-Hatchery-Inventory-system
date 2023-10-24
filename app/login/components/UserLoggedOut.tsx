ba9a72613def886798aa021e600d26fc0729a583
lokisk1155 <shawn12345d@gmail.com>
2023-10-24 09:09:41 -0400

    login as user or admin

--- app/login/components/UserLoggedOut.tsx
+++ app/login/components/UserLoggedOut.tsx
@@ -1,20 +1,37 @@
 'use client'
+import { Role } from 'interfaces/session'
 import { signIn } from 'next-auth/react'
 import { MouseEvent } from 'react'
+import Cookies from 'js-cookie'
 
 export default function UserLoggedOut() {
-  const handleSignIn = async (e: MouseEvent<HTMLButtonElement>) => {
+  const handleSignInAdmin = async (e: MouseEvent<HTMLButtonElement>) => {
     e.preventDefault()
-    await signIn('google')
+    await signIn('google', {
+      callbackUrl: `${window.location.origin}/api/auth/callback/google?role=${Role.ADMIN}`,
+    })
+  }
+  const handleSignInUser = async (e: MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault()
+    Cookies.set('wantedRole', Role.USER)
+    await signIn('google', {
+      callbackUrl: `${window.location.origin}/api/auth/callback/google?role=${Role.USER}`,
+    })
   }
   return (
-    <>
+    <div className="w-full flex-col items-start">
       <button
-        className="w-1/2 text-3xl hover:underline hover:bg-gray-200 dark:hover:bg-gray-800 border-solid border-[3px]"
-        onClick={handleSignIn}
+        className="w-full text-3xl hover:underline hover:bg-gray-200 dark:hover:bg-gray-800 border-solid border-[3px]"
+        onClick={handleSignInAdmin}
       >
-        login
+        login as admin
       </button>
-    </>
+      <button
+        className="w-full text-3xl hover:underline hover:bg-gray-200 dark:hover:bg-gray-800 border-solid border-[3px] mt-4"
+        onClick={handleSignInUser}
+      >
+        login as user
+      </button>
+    </div>
   )
 }
